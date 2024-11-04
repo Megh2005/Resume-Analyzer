398274419bdbb807a6b8dc99cae849dfd925ec65
Megh Deb <megh.deb.civil27@heritageit.edu.in>
2024-11-04 16:20:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import pdfplumber
 import os
 
 # Configure the Gemini API key using environment variable
-api_key = os.getenv("API_KEY")
+api_key = "AIzaSyCpMOrythR3tdbgu5zBHdzQcOzxZNS5-hE"
 
 # Set custom page configuration with a favicon and layout settings
 st.set_page_config(
